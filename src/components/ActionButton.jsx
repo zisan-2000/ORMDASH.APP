9fc0ac29b7328bc14fbd3e2cc44c48aeef16d94e
MD. ZULFIKER HOSSAIN <zisan19393871@gmail.com>
2024-09-09 17:44:05 +0600

    sticky header

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const ActionButton = () => {
   return (
     <Link to="/create-feed">
-      <button className="flex items-center gap-2 rounded-md bg-blue-500 px-6 py-3 text-white shadow-md hover:bg-blue-600 sm:px-8 sm:py-4 md:px-10 md:py-5 lg:px-12 lg:py-5 xl:px-14 xl:py-5 2xl:ml-96">
+      <button className="flex items-center gap-2 rounded-md bg-blue-500 px-6 py-3 text-white shadow-md hover:bg-blue-600 sm:px-8 sm:py-4 md:px-10 md:py-5 lg:px-12 lg:py-5 xl:px-14 xl:py-5 2xl:ml-60">
         <FaPlus /> {/* Left-aligned icon */}
         Create New Feed
       </button>
--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TextSection = () => {
   return (
-    <div className="text-center 2xl:ml-96">
+    <div className="text-center xl:ml-72 2xl:ml-64">
       <h2 className="mb-2 text-xl font-semibold text-blue-500 sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl">
         Nothing here yet
       </h2>
